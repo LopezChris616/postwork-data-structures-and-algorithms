55955d2a830f6a163aebfaec8da25f4ca27b1fad
Christian Lopez <lopezchristian1610@gmail.com>
2023-05-20 01:09:01 -0400

    Week 1 - Day 2 - Find 1st Duplicate Complete

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,14 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let arrNums = new Set();
+  for(let i = 0; i < arr.length; i++) {
+    if(arrNums.has(arr[i])) {
+      return arr[i];
+    } else {
+      arrNums.add(arr[i]);
+    }
+  }
+  return -1;
 }
 
 if (require.main === module) {
@@ -11,9 +20,47 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("");
+
+  console.log("Expecting: 8");
+  console.log("=>", findFirstDuplicate([3, 8, 1, 9, 8, 4, 7, 1]));
+
+  console.log("");
+
+  console.log("Expecting: -1");
+  console.log("=>", findFirstDuplicate([2, 6, 1, 7, 4]));
 }
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+
+/*
+Rewrite the problem in your own words
+  Given an array I need to check and return the first value that is duplicated
+  So if the array is, [3, 8, 1, 9, 8, 4, 7, 1], it should return 8 because
+  the value 8 is in two elements of the arr. It should not return 1 also because
+  while it is also duplicating, it is not the first number that is duplicated.
+  if the array does not have any duplicate values then it should return -1.
+  So if the array is, [2, 6, 1, 7, 4], it should return -1 because there
+  are not repeated values
+*/
+
+/* Please add your pseudocode to this file
+  initialize a new set to keep track of all nums in arr
+  loop through array
+    if set has num
+      return num
+    add num to set if num not found
+  return -1 if no match found
+
+*/
+
+/* And a written explanation of your solution
+    I used a set as a way of keeping track of all the nums encountered
+    I then looped through the array checking to see of the num in each
+    iteration is a part of the set. if it is, then I simply return the num
+    if not then I add that num to the set to check for duplicates in future iterations
+    exiting the loop I return -1, since if it reaches that point then there are no
+    duplicates in the array
+*/
